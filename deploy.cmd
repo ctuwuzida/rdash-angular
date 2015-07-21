e25287f290b6fc797b9cd7f953e98c67018e2183
Stanley Wu <bleedtodry@hotmail.com>
2015-07-21 22:26:46 +0800

    fix kudu

--- deploy.cmd
+++ deploy.cmd
@@ -88,39 +88,21 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
-echo 1. Select node version
-call :SelectNodeVersion
-
-echo 2. Install npm packages
-IF EXIST "package.json" (
-  echo Calling !NPM_CMD! install --production
-  pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd npm install --production -d
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
-
-echo 3. Install bower packages
-IF EXIST "bower.json" (
-  pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd bower install
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
-
-echo 4. Gulp build
-IF EXIST "gulpfile.js¡± (
-  pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd grunt build
-  call :ExecuteCmd grunt
-  IF !ERRORLEVEL! NEQ 0 goto error
-  popd
-)
-
-echo 5. KuduSync
+:: 1. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_SOURCE%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
   IF !ERRORLEVEL! NEQ 0 goto error
+)
+
+:: 2. Select node version
+call :SelectNodeVersion
+
+:: 3. Install npm packages
+IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
+  pushd "%DEPLOYMENT_TARGET%"
+  call :ExecuteCmd !NPM_CMD! install --production
+  IF !ERRORLEVEL! NEQ 0 goto error
+  popd
 )
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
