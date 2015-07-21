8b9121c4bbbbe07826cca603a0a7903697a805ce
Stanley Wu <bleedtodry@hotmail.com>
2015-07-21 10:57:55 +0800

    fix kudu

--- deploy.cmd
+++ deploy.cmd
@@ -92,7 +92,7 @@ echo 1. Select node version
 call :SelectNodeVersion
 
 echo 2. Install npm packages
-IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
+IF EXIST "package.json" (
   pushd "%DEPLOYMENT_SOURCE%"
   call :ExecuteCmd !NPM_CMD! install --production -d
   IF !ERRORLEVEL! NEQ 0 goto error
